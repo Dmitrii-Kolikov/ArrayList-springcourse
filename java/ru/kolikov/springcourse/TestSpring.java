204906666d82b4baa480d49c7528de84b59739e8
Dmitrii <74898966+Dmitrii-Kolikov@users.noreply.github.com>
2020-12-02 12:58:39 +0700

    Update TestSpring.java

--- java/ru/kolikov/springcourse/TestSpring.java
+++ java/ru/kolikov/springcourse/TestSpring.java
@@ -7,11 +7,11 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicattionContext.xml"
         );
-            //Music music = context.getBean("musicBean", Music.class);
-            //MusicPlayer musicPlayer = new MusicPlayer(music);
+        
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusicList();
-        context.close();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
     }
 }
