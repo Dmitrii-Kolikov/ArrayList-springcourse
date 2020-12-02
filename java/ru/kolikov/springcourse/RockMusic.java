a6cfbb9f0d2ae5147c4910d75ce445b22a790b13
Dmitrii <74898966+Dmitrii-Kolikov@users.noreply.github.com>
2020-12-02 18:22:26 +0700

    Update RockMusic.java

--- java/ru/kolikov/springcourse/RockMusic.java
+++ java/ru/kolikov/springcourse/RockMusic.java
@@ -1,6 +1,12 @@
 package ru.kolikov.springcourse;
 
 public class RockMusic implements Music {
+    public void doMyInit() {
+        System.out.println("Doing my initialization in the class RockMusic");
+    }
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction in the class RockMusic");
+    }
     @Override
     public String getSong() {
         return "The Wind cries Mary";
