36084a018d67cfef92af3a01239a47d155acacb0
Dmitrii <74898966+Dmitrii-Kolikov@users.noreply.github.com>
2020-12-03 18:36:33 +0700

    Update RapMusic.java

--- java/ru/kolikov/springcourse/RapMusic.java
+++ java/ru/kolikov/springcourse/RapMusic.java
@@ -1,12 +1,6 @@
 package ru.kolikov.springcourse;
 
 public class RapMusic implements Music {
-    public void doMyInit() {
-        System.out.println("Doing my initialization in the class RapMusic");
-    }
-    public void doMyDestroy() {
-        System.out.println("Doing my destruction in the class RapMusic");
-    }
     @Override
     public String getSong() {
         return "8 mile";
