40ff207b13a806059d1917680666f80595f7cca7
Dmitrii <74898966+Dmitrii-Kolikov@users.noreply.github.com>
2020-12-03 19:25:36 +0700

    Update TestSpring.java

--- java/ru/kolikov/springcourse/TestSpring.java
+++ java/ru/kolikov/springcourse/TestSpring.java
@@ -10,9 +10,9 @@ public class TestSpring {
         MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer1.playMusicList();  // проверям наши init-method and destroy-method.
         MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer2.playMusicList();  // проверям наши init-method and destroy-method при  созданни несколько бинов Singleton с одинаковым объектом.
+        musicPlayer2.playMusicList();  // проверям наши init-method and destroy-method при  получение несколько бинов Singleton с одинаковыми объектами.
         //смотрим как происходит срабатывание метода init и destroy и выводим весь наш список на экран.
-        //Scope Singleton вызывает метод init и destroy, только один раз, при условие создание этих объектов в одном классе.
+        //Scope Singleton вызывает метод init и destroy, только один раз, но если мы создадим в каком-то классе еще раз эти методы, то будет вывод уже дважды, потому что мы используем ArrayList.
        
 
 
